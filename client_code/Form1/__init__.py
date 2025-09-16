5bc1db82220205e9636f913f13a0377354a5f868
ruby.hemingway-gordon@student.uts.edu.au <ruby.hemingway-gordon@student.uts.edu.au>
2025-09-16 05:30:58 +0000

    Created BrandAssets_Logos_01-Wordmark.jpg and download.png; Edited theme/parameters.yaml and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -4,10 +4,17 @@ class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    # netflix button code
-    self.button_1.text = "Netflix"
-    self.button_1.background = "Black"
-    self.button_1.foreground = "Red"
+  def button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
+
+  def button_3_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
+
+  def button_4_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
     
  
 
