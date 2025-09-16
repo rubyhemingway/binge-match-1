9b0bac97117837e4b1afea75a8d0b7ec8197a5f6
ruby.hemingway-gordon@student.uts.edu.au <ruby.hemingway-gordon@student.uts.edu.au>
2025-09-16 04:25:24 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -5,12 +5,11 @@ class Form1(Form1Template):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     # netflix button code
-    self.sidebar_button_1.background = "black"
-    self.sidebar_button_1.foreground = "red"
-
-  def sidebar_button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    pass
+    self.button_1.text = "Netflix"
+    self.button_1.background = "Black"
+    self.button_1.foreground = "Red"
+    
+ 
 
 
 
