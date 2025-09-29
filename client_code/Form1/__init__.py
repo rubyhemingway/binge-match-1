8a341337b615fd3c76cefcf4cad45d0957e12b4e
ruby.hemingway-gordon@student.uts.edu.au <ruby.hemingway-gordon@student.uts.edu.au>
2025-09-29 09:26:59 +0000

    Created Form1.ItemTemplate1; Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,24 +1,34 @@
+#anvil template
 from ._anvil_designer import Form1Template
 from anvil import *
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-  def button_2_click(self, **event_args):
+
+  #focus to search bar
+  def outlined_button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
+    self.search_bar.focus()
     pass
-    self.text_box_1.focus()
-
-  def button_3_click(self, **event_args):
-    """This method is called when the button is clicked"""
+  #search bar basic responses
+  def search_bar_click(self, **event_args):
     pass
 
-  def button_4_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    pass
-    
- 
+  def search_bar_pressed_enter(self, **event_args):
+    query = self.search_bar.text.strip().lower()
+
+  responses = {
+    "happy": "Brooklyn Nine-Nine",
+    "sad": "13 Reasons Why",
+    "exciting": ["Wednesday", "Bridgerton"]
+
+  }
+result = responses.get(query, f"No recommendation for '{query}'")
+self.output_label.text = result
+"""This method is called when the user presses Enter in this text box"""
+pass
 
 
 
-  
+
