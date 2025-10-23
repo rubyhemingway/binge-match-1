710cc6dc9282619dfa3285564ad83d374eaa96bb
ruby.hemingway-gordon@student.uts.edu.au <ruby.hemingway-gordon@student.uts.edu.au>
2025-10-23 05:37:35 +0000

    Created happyfeet titlepage.jpeg and netflixpage.happyfeet; Edited netflixpage.ItemTemplate2

--- client_code/netflixpage/ItemTemplate2/__init__.py
+++ client_code/netflixpage/ItemTemplate2/__init__.py
@@ -1,15 +1,21 @@
 from ._anvil_designer import ItemTemplate2Template
 from anvil import *
 
+# to make items appear in the repeating panel, that are title names they needed to be coded in a different template
+# i had to use copilot to figure this part out, but once i got it it was pretty intuiative.
 class ItemTemplate2(ItemTemplate2Template):
   def __init__(self, **properties):
     self.init_components(**properties)
     self.button_1.text = self.item['title']
 
+# defining the item
   def set_item(self, item):
     self.item = item
 
+# this is the code for the buttons which appear in the panel when the user presses enter.
+# clicking on the title names will take the user to a review page of that title.
+# same open form tutorial from anvil.works used.
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
-    pass
+    open_form('netflixpage.happyfeet')
    
---
+++ client_code/netflixpage/happyfeet/__init__.py
@@ -0,0 +1,40 @@
+from ._anvil_designer import happyfeetTemplate
+from anvil import *
+
+# anvil given template which is required in order for code written to run properly.
+class happyfeet(happyfeetTemplate):
+  def __init__(self, **properties):
+    self.init_components(**properties)
+
+  # takes user back to netflix page.
+  # anvil tutorial used for this code.
+  def outlined_button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('netflixpage')
+
+  # takes the user back to the homepage of the site.
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('homepage')
+
+  # focuses the users mouse to the text box to write a review on the movie.
+  def outlined_button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.reviewbox_1.focus()
+
+# allows the user to write a review which gets posted on the site.
+  def reviewbox_1_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    user_input = self.reviewbox_1.text.strip()
+    self.reviewbox_1.text = ""
+
+# this is the code for the label which appears under the review box when the user presses enter.
+# this code is for the label to be visible or not.
+# i used what I learnt when creating the search bar to help me write this code.
+    if user_input:
+      self.output_label.text = user_input
+      self.output_label.visible = True
+    else:
+      self.output_label.text = ""
+      self.output_label.visible = False
+
